c55bad1927fea9e10009fb2eaea98314f17aa914
Ayush-Mishra2812 <ayush2812mishra@gmail.com>
2025-03-25 10:20:30 +0530

    completed variables

--- c/Learning_Variables/variables.c
+++ c/Learning_Variables/variables.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    char name = 'John';
-    int age = '50';
+    char name[] = "John";
+    int age = 50;
 
-    printf("There one was a man names %s", name);
-    printf("He was %d years old", age);
-    printf("He really liked the name %s", name);
-    printf("But he did not like that he was %d years old", age);
+    printf("There one was a man names %s \n", name);
+    printf("He was %d years old \n", age);
+    printf("He really liked the name %s \n", name);
+    printf("But he did not like that he was %d years old \n", age);
 }
