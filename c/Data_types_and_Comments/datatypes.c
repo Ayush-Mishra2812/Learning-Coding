7dea76801de3c57afba6876e000f63d4f3818ce2
Ayush-Mishra2812 <ayush2812mishra@gmail.com>
2025-03-25 12:05:50 +0530

    comments and data types done

---
+++ c/Data_types_and_Comments/datatypes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {   
+    int age = 14;                               /*used to store integers*/
+    double percentage = 99.9;                   /*used to store decimals and has double the sace compared to float*/
+    char grade = 'A';                           /*used to store single characters i.e. letters*/
+    char remark[] = 'A very bright student';    /*used to store a string, its a a modification of a character*/
+}
