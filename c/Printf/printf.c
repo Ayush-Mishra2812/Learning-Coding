c69e05396156e3b582e8d79f00270b9ab7a7964b
Ayush-Mishra2812 <ayush2812mishra@gmail.com>
2025-03-25 12:34:46 +0530

    printf explored

---
+++ c/Printf/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("My %s is 211769727 \n","roll number is");            /* %s is used for strings*/
+    printf("My roll number is %d \n", 21169727);                 /* %d is used for integers*/
+    printf("The first few digits of pi are %f \n", 3.141592653); /* %f is used for floats (decimals)*/
+    printf("%c comes after %c \n", 'B', 'A');                    /*%c is used for characters*/
+
+
+    /*we can use different types of data is the same printf command by sperating them by commas according to 
+    their order in the string of the printf command*/
+}
