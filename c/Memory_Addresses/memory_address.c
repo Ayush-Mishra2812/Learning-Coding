d65755260f2bc5e44db9d4156e063b50028fca74
Ayush-Mishra2812 <ayush2812mishra@gmail.com>
2025-03-27 17:36:43 +0530

    memory address

--- c/Memory_Addresses/memory_address.c
+++ c/Memory_Addresses/memory_address.c
@@ -3,6 +3,10 @@
 
 int main(){
 
-    //
+    int age = 15;
+    float pi = 3.14;
+
+    printf("pi: %p \n", &pi);           //tells us the memory address where the variable pi is stored
+    printf("age: %p \n", &age);          //tells us the memory address where the variable age is stored
 
 }
