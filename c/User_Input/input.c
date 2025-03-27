8730d26c6afcd9b749da6fe8d2a9a49361bcae6b
Ayush-Mishra2812 <ayush2812mishra@gmail.com>
2025-03-27 11:40:28 +0530

    Input complete

--- c/User_Input/input.c
+++ c/User_Input/input.c
@@ -7,4 +7,17 @@ int main() {
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("you are %d years old \n", age);
+
+
+    char name[30];                              
+    printf("What is your name: ");
+    scanf("%s", name);                     /*Cant store strings which contain a space between them*/     
+    printf("Your name is %s \n", name);
+
+    char name[30];                              
+    printf("What is your name: ");
+    fgets(name, 30, stdin);                          /*Can store a string which contains a space between them*/
+    printf("Your name is %s \n", name);
+
+
 }
