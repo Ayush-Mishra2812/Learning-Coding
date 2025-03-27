46d1e61c8e6f54c342e7de0f746806c021d9e1b7
Ayush-Mishra2812 <ayush2812mishra@gmail.com>
2025-03-27 12:58:34 +0530

    learned Arrays

---
+++ c/Arrays/arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //Pre-defined array
+    int randomnumber[] = {1,2,3,4,5,6,7,8,9,10}; // all are designated a position starting from 0 till n
+                        //0 1 2 3 4 5 6 7 8  9
+    randomnumber[9] = 11;                        // We can change the value of any position of the array
+
+
+
+    //Defining array
+    int list[3];
+    list[0] = 1;        // we can add/cange the value of any postiton on the array afterwards
+    list[1] = 32;
+    list[2] = 332;
+
+}
